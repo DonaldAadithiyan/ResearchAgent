e565e82245ec56e98ab54ba91acf08ef9751e349
DonaldAadithiyan <donaldaadithiyanwork@gmail.com>
2025-08-14 14:12:12 +0530

    got raw response

--- main.py
+++ main.py
@@ -2,15 +2,52 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
-# from langchain_core.prompts import ChatPromptTemplate
-# from langchain_core.output_parsers import PydanticOutputParser
-# from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 # from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 
+class AgentResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.0)
 llm2 = ChatAnthropic(model="claude-2", temperature=0.0)
 
-response = llm.invoke("What is the capital of France?")
-print(response.content)
+parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())      
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    tools=[],
+    prompt=prompt
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=[],
+    verbose=True)
+
+raw_response = agent_executor.invoke({
+    "query": "What is the impact of climate change on global agriculture?"})
+print(raw_response)
+    
